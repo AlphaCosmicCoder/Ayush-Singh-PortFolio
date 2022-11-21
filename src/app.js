e239120ba208ec74a6ce4ca2b4179d7e9e500b81
Ayush Kumar Singh <83543768+099ayuh@users.noreply.github.com>
2022-11-22 03:18:46 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -25,21 +25,46 @@ const changeMenuIcon = () => {
 }
 
 
-
 // toggle theme 
 const html = document.querySelector('html');
 const toggleTheme = document.querySelector('#toggle-theme');
 const toggleThemeIcon = toggleTheme.querySelector('img');
 
+const darkMode = () => {
+    html.classList.toggle('dark')
+
+    if (setDarkMode !== "on") {
+        // Set the value of the itwm to "on" when dark mode is on
+        document.getElementById('figureID').style.filter = "drop-shadow(0 0.6rem 0.5rem #022b41ac)";
+    } else {
+        // Set the value of the item to  "null" when dark mode if off
+        document.getElementById('figureID').style.filter = "drop-shadow(0 0.6rem 0.5rem #b4bfb7)";
+    }
+}
+
 toggleTheme.addEventListener('click', () => {
-    html.classList.toggle('dark');
+    // Get the value of the "dark" item from the local storage on every click
+    setDarkMode = localStorage.getItem('dark');
+
+    if (setDarkMode !== "on") {
+        darkMode();
+        // Set the value of the itwm to "on" when dark mode is on
+        setDarkMode = localStorage.setItem('dark', 'on');
+        document.getElementById('figureID').style.filter = "drop-shadow(0 0.6rem 0.5rem #b4bfb7)";
+    } else {
+        darkMode();
+        // Set the value of the item to  "null" when dark mode if off
+        document.getElementById('figureID').style.filter = "drop-shadow(0 0.6rem 0.5rem #022b41ac)";
+        setDarkMode = localStorage.setItem('dark', null);
+    }
+
     const isDark = html.classList.contains('dark');
     const themeIcon = isDark ? 'light' : 'dark';
     toggleThemeIcon.src = `./images/icons/icon-${themeIcon}.svg`;
 })
 
 // typewriter effect
-var TxtType = function(el, toRotate, period) {
+var TxtType = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
@@ -49,17 +74,17 @@ var TxtType = function(el, toRotate, period) {
     this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
+TxtType.prototype.tick = function () {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
 
     if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
     var that = this;
     var delta = 200 - Math.random() * 100;
@@ -67,26 +92,36 @@ TxtType.prototype.tick = function() {
     if (this.isDeleting) { delta /= 2; }
 
     if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
+        delta = this.period;
+        this.isDeleting = true;
     } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
     }
 
-    setTimeout(function() {
-    that.tick();
+    setTimeout(function () {
+        that.tick();
     }, delta);
 };
 
-window.onload = function() {
+
+
+// Get the value of the "dark" item from the local storage
+let setDarkMode = localStorage.getItem('dark');
+
+// Check dark mode is on or off on page reload
+if (setDarkMode === 'on') {
+    darkMode();
+}
+
+window.onload = function () {
     var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
         var period = elements[i].getAttribute('data-period');
         if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
+            new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
     // INJECT CSS
